b417ac4461ce97d89a138b975f054e57f940d4cb
Sarah Lohmeier <sarahlohmes@gmail.com>
2024-09-14 19:13:08 +0000

    Redirect to old site

--- server.js
+++ server.js
@@ -4,9 +4,10 @@ const hostname = '127.0.0.1';
 const port = 3000;
 
 const server = createServer((req, res) => {
-  res.statusCode = 200;
+  res.statusCode = 307; // redirect
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
+  res.setHeader('Location', 'https://slohmes.wordpress.com/');
+  res.end('New site under construction, redirecting to old site for now.');
 });
 
 server.listen(port, hostname, () => {
