4b55c144d8d1bc32d2b2efca709765c45f5c7543
Sarah Lohmeier <sarahlohmes@gmail.com>
2024-09-14 21:05:39 +0000

    set default port to 8080

--- Dockerfile
+++ Dockerfile
@@ -3,8 +3,5 @@
 FROM node:22-alpine
 WORKDIR /app
 COPY . .
-RUN echo "### current PORT value=${PORT}"
-ENV PORT=${PORT}
-RUN echo "### creating new port var=${PORT}"
 RUN yarn install --production
 CMD ["node", "src/server.js"]
--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ const { createServer } = require('node:http');
 
 const hostname = '127.0.0.1';
 
-const port = (!!process.env.PORT && process.env.PORT.length > 0) ? process.env.PORT : 3000;
+const port = (!!process.env.PORT && process.env.PORT.length > 0) ? process.env.PORT : 8080;
 console.log(`### process.env.PORT=${process.env.PORT}, port=${port}`)
 
 const server = createServer((req, res) => {
